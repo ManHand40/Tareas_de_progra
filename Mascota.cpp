cf20a7c1d0d835e188e4eb633ba9d3a2883dec25
ManHand40 <joseluisesquivel059@gmail.com>
2025-03-26 20:34:25 -0600

    YO
    Estoy probando algo

---
+++ Mascota.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Mascota
+{
+private:
+    /* data */
+public:
+    Mascota(/* args */);
+    ~Mascota();
+};
+
+Mascota::Mascota(/* args */)
+{
+}
+
+Mascota::~Mascota()
+{
+}
+
+int main(){
+
+    return 0;
+}
